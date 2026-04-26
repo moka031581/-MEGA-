ca857f83776ca733ca56addd1401b7b246902248
moka031581 <mo.ka031581@gmail.com>
2026-04-26 16:05:04 +0900

    Update check.py

--- check.py
+++ check.py
@@ -3,4 +3,8 @@ import os
 
 WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
-requests.post(WEBHOOK, json={"content": "🔥強制テスト通知"})
+print("Webhook:", WEBHOOK)  # ← まず確認
+
+res = requests.post(WEBHOOK, json={"content": "🔥テスト通知"})
+print("status:", res.status_code)
+print("response:", res.text)
