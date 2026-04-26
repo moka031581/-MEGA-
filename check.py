2aae7b787aec9637751fa2da7ebbed2bbf9dbc7c
moka031581 <mo.ka031581@gmail.com>
2026-04-26 16:15:49 +0900

    Update check.py

--- check.py
+++ check.py
@@ -1,10 +1,25 @@
 import requests
 import os
 
+URL = "https://www.toysrus.co.jp/ja-jp/pokemoncardgame/"
 WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
-print("Webhook:", WEBHOOK)  # ← まず確認
+def send_discord(msg):
+    if WEBHOOK:
+        res = requests.post(WEBHOOK, json={"content": msg})
+        print("status:", res.status_code)
+    else:
+        print("Webhook is None")
 
-res = requests.post(WEBHOOK, json={"content": "🔥テスト通知"})
-print("status:", res.status_code)
-print("response:", res.text)
+def check():
+    res = requests.get(URL)
+    text = res.text
+
+    if "MEGAドリームex" in text:
+        if ("在庫あり" in text) or ("カートに入れる" in text):
+            send_discord("🔥【在庫あり】MEGAドリームex")
+        else:
+            send_discord("👀【掲載】MEGAドリームex（在庫不明）")
+
+if __name__ == "__main__":
+    check(send_discord("🔥テスト通知"))
